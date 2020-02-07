484852985b5fea080924badc05bdbb0821bcbf5e
akshaysorathiya9581 <akshaysorathiya555@gmail.com>
2020-02-07 17:23:08 +0530

    passport client join add

--- app/Passport.php
+++ app/Passport.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Passport extends Model
 {
-    //
+    public function clientDetails()
+    {
+    	return $this->hasOne('App\MasterClient','id','client_id');
+    }
 }
